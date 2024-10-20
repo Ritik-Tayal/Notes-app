241f016ce0780e5f51af4c31f78b67037b6684b5
Ritik-Tayal <ritik23153049@akgec.ac.in>
2024-10-20 23:58:19 +0530

    Create Functionality Added

--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Notes
+from .forms import NoteForm
 
 # Create your views here.
+
+def create(request):
+    form=NoteForm()
+    if request.method=='POST':
+        form=NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('notes.list')
+            
+    return render(request,'notes/newNote.html',{'form':form})
+
 def list(request):
     all_notes=Notes.objects.all()
     return render(request,'notes/notesList.html',{'notes': all_notes})
