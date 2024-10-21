7a6076c6fd6aaf2fe291a2d0e8a648569fcf0d37
Ritik-Tayal <ritik23153049@akgec.ac.in>
2024-10-21 22:06:21 +0530

    changed requirements

--- notes_app/notes_app/settings.py
+++ notes_app/notes_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8s@%a6w#$!0q=^8q$iml7nwea%_srjd=gh)!9pwj03*co9thw1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
